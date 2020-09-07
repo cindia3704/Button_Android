6b8d46da9a287f6e70fa58140d4e9a95b8db0fd3
김지수 <jisookim@gimjisuui-MacBookPro.local>
2020-09-07 19:58:33 +0900

    findEmail implementation

--- app/src/main/java/com/example/button/startApp_1/FindEmailActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/FindEmailActivity.kt
@@ -3,18 +3,79 @@ package com.example.button.startApp_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.button.R
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_find_email.*
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class FindEmailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_email)
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://18.191.146.76:9999/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        yes_to_find_id.setOnClickListener {
+            val service = retrofit.create(RetrofitService_Login::class.java)
+            var userEmail = enter_email_to_find_id.text.toString()
+
+            service.findUserEmail(userEmail.toString()).enqueue(object : Callback<ExistsOrNot> {
+                override fun onResponse(call: Call<ExistsOrNot>, response: Response<ExistsOrNot>) {
+                    if (response.isSuccessful) {
+                        var exists_text = response.body()!!.exists
+                        Log.d("emailtext",exists_text.toString())
+                        var exists_string = exists_text.toString()
+                        Log.d("eText", exists_text.toString())
+                        if (exists_string.equals("true")) {
+                            Toast.makeText(
+                                this@FindEmailActivity,
+                                "해당 아이디가 존재합니다",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@FindEmailActivity,
+                                "해당 아이디는 존재하지 않습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@FindEmailActivity,
+                            "해당 아이디는 존재하지 않습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ExistsOrNot>, t: Throwable) {
+                    Log.d("error", "에러"+t.toString())
+                    Toast.makeText(
+                        this@FindEmailActivity,
+                        "실패!!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+        }
+
         go_back_to_login.setOnClickListener({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         })
     }
+
+}
+class ExistsOrNot(var exists:String){
+
 }
