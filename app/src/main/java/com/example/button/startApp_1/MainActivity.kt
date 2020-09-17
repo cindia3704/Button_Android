73f2317ad6f5340f0abee88dbae4d9c0bd36ea3a
sO-Omin <ghdrl9910@gmail.com>
2020-09-18 00:52:56 +0900

    RecyclerView

--- app/src/main/java/com/example/button/startApp_1/MainActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/MainActivity.kt
@@ -2,33 +2,44 @@ package com.example.button.startApp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.button.R
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_navigation_tab.*
+import kotlinx.android.synthetic.main.fragment_mycloset.*
+import kotlinx.android.synthetic.main.mycloset_category.*
 import java.lang.reflect.Array.set
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath: String
+
 
     private fun configureBottomNavigation() {
         vp_ac_main_frag_pager.adapter = MainFragmentStatePagerAdapter(supportFragmentManager, 3)
         tl_ac_main_bottom_menu.setupWithViewPager(vp_ac_main_frag_pager)
 
-        val bottomNaviLayout: View = this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
+        val bottomNaviLayout: View =
+            this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
         tl_ac_main_bottom_menu.getTabAt(0)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_recommend_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(1)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_mycloset_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(2)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_calendar_tab) as RelativeLayout
-
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -37,5 +48,12 @@ class MainActivity : AppCompatActivity() {
 
         configureBottomNavigation()
         vp_ac_main_frag_pager.setCurrentItem(1)
+
+
     }
+
+
 }
+
+
+
--- app/src/main/java/com/example/button/startApp_1/MyclosetFragment.kt
+++ app/src/main/java/com/example/button/startApp_1/MyclosetFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.button.R
+
 
 
 class MyclosetFragment : Fragment() {
@@ -14,7 +17,8 @@ class MyclosetFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_mycloset, container, false)
     }
-
 }
+
+
