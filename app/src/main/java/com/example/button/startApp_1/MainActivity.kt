5b595544e77f9d8657b8530f7911bb8f6051f770
김지수 <jisookim@gimjisuui-MacBookPro.local>
2020-09-18 12:16:54 +0900

    change myCloset xml

--- app/src/main/java/com/example/button/startApp_1/MainActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.button.startApp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -19,6 +23,8 @@ import kotlinx.android.synthetic.main.bottom_navigation_tab.*
 import kotlinx.android.synthetic.main.fragment_mycloset.*
 import kotlinx.android.synthetic.main.mycloset_category.*
 import java.lang.reflect.Array.set
+import com.example.button.startApp_1.Clothes
+import com.example.button.startApp_1.ClothList
 
 class  MainActivity : AppCompatActivity() {
 
@@ -46,14 +52,44 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        val clothLists=ArrayList<Clothes>()
+//        for(i in 0 until 5){
+//            clothLists.add(Clothes(name=""+i+"번째 옷"))
+//        }
+//        val adapter=clothAdapter(clothLists, LayoutInflater.from(this@MainActivity))
+//        recyclerView_category.adapter=adapter
+//        recyclerView_category.layoutManager=LinearLayoutManager(this@MainActivity)
+
         configureBottomNavigation()
         vp_ac_main_frag_pager.setCurrentItem(1)
 
-
     }
 
+    // 옷 객체 만들어서 옷 리스트에 추가하는 함수
+//    fun createFakeClothList(numberOfItems:Int=5, clothList:ClothList= ClothList()):ClothList{
+//        for(i in 0 until numberOfItems){
+//            clothList.addClothes(
+//                Clothes()
+//            )
+//        }
+//        return clothList
+//    }
+//
+//    // 위에서 만든 옷 리스트를 실제 뷰에 추가하기
+//    fun createClothList(clothList:ClothList){
+//        val layoutInflater=LayoutInflater.from(this@MainActivity)
+//        val container=findViewById<recyclerView >(R.id.clothList_view)
+//        for(i in 0 until clothList.list.size){
+//            val viewOfCloth=layoutInflater.inflate(R.layout.mycloset_clothes,null)
+//            val clothView=viewOfCloth.findViewById<ImageView>(R.id.cloth_image)
+////            val clothTextView=viewOfCloth.findViewById<TextView>(R.id.cloth_text)
+////            clothTextView.setText(clothList.list.get(i).name)
+//            container.addView(clothView)
+//        }
+//    }
 
 }
+
 
 
 
--- app/src/main/java/com/example/button/startApp_1/MyclosetFragment.kt
+++ app/src/main/java/com/example/button/startApp_1/MyclosetFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.ListAdapter
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.button.R
-
-
+import kotlinx.android.synthetic.main.fragment_mycloset.*
+import com.example.button.startApp_1.Clothes
 
 class MyclosetFragment : Fragment() {
     override fun onCreateView(
@@ -19,6 +24,45 @@ class MyclosetFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_mycloset, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val clothLists=ArrayList<Clothes>()
+        // 임의로 5개의 옷 만듬
+        for(i in 0 until 5){
+            clothLists.add(Clothes(name=""+i+"번째 옷"))
+        }
+        val adapter=clothAdapter(clothLists, LayoutInflater.from(activity))
+        recyclerView_category!!.adapter=adapter
+        recyclerView_category!!.layoutManager=LinearLayoutManager(activity)
+    }
 }
 
+class clothAdapter(
+    val itemList:ArrayList<Clothes>,
+    val inflater: LayoutInflater
+): RecyclerView.Adapter<clothAdapter.ViewHolder>(){
+    class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        val clothImage:ImageView
+        val clothText: TextView
 
+        init {
+             clothImage=itemView.findViewById(R.id.cloth_image)
+            clothText=itemView.findViewById(R.id.cloth_text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view=inflater.inflate(R.layout.mycloset_clothes,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.clothText.setText(itemList.get(position).name)
+//        holder.clothImage.setImageURI(itemList.get(position).image)
+    }
+}
