c438deaf97c2109c826b3146ffe6a33a1d14b0c7
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-04 11:57:46 +0900

    Update User.kt

--- app/src/main/java/com/example/button/startApp_1/data/User.kt
+++ app/src/main/java/com/example/button/startApp_1/data/User.kt
@@ -1,3 +1,9 @@
 package com.example.button.startApp_1.data
 
-data class User (val id : Int, val password : String, val userEmail:String, val userNickName:String, val userGender : String)
+data class User(
+    val id : Int,
+    val password : String,
+    val userEmail:String,
+    val userNickName:String,
+    val userGender : String
+)
