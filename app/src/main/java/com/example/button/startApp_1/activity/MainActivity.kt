b87e1a6696dd2c0f1f26c35e41a8bbb1317b2a0b
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-09 12:46:38 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/button/startApp_1/activity/MainActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/activity/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.example.button.startApp_1.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.button.R
 import com.example.button.startApp_1.adapter.MainFragmentStatePagerAdapter
-import com.example.button.startApp_1.fragment.MyclosetFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class  MainActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 1
-    lateinit var currentPhotoPath: String
     var userId: Int=1
 
-    private fun configureBottomNavigation() {
+    private fun configureBottomNavigation(userId : Int) {
         vp_ac_main_frag_pager.adapter =
             MainFragmentStatePagerAdapter(
                 supportFragmentManager,
-                3
+                5,
+                userId
             )
         tl_ac_main_bottom_menu.setupWithViewPager(vp_ac_main_frag_pager)
 
@@ -33,60 +29,20 @@ class  MainActivity : AppCompatActivity() {
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_mycloset_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(2)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_calendar_tab) as RelativeLayout
-    }
-    fun getUsersId():Int{
-        return this.userId
+        tl_ac_main_bottom_menu.getTabAt(3)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_calendar_tab) as RelativeLayout
+        tl_ac_main_bottom_menu.getTabAt(4)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_calendar_tab) as RelativeLayout
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //var intent = Intent(this@MainActivity,LoginActivity::class.java)
-         userId=getIntent().getIntExtra("userId",3)
-        Log.d("user id from login:",""+userId)
-
         super.onCreate(savedInstanceState)
-//        val bundle = Bundle().apply {
-//            putInt("userId", userId)
-//        }
-
-//        var intent2=Intent(this@MainActivity,MyclosetFragment::class.java)
-//        intent2.putExtra("userId",userId)
-//        startActivity(intent2)
-//        val fragment = MyclosetFragment.newInstance(userId)
-        //fragment.arguments= bundle
-       // Log.d("user bundle in main:",""+bundle["userId"])
-        //MyclosetFragment.newInstance(userId)
         setContentView(R.layout.activity_main)
+        userId=getIntent().getIntExtra("userId",3)
 
-
-        configureBottomNavigation()
+        configureBottomNavigation(userId)
         vp_ac_main_frag_pager.setCurrentItem(1)
     }
-
-
-
-    // 옷 객체 만들어서 옷 리스트에 추가하는 함수
-//    fun createFakeClothList(numberOfItems:Int=5, clothList:ClothList= ClothList()):ClothList{
-//        for(i in 0 until numberOfItems){
-//            clothList.addClothes(
-//                Clothes()
-//            )
-//        }
-//        return clothList
-//    }
-//
-//    // 위에서 만든 옷 리스트를 실제 뷰에 추가하기
-//    fun createClothList(clothList:ClothList){
-//        val layoutInflater=LayoutInflater.from(this@MainActivity)
-//        val container=findViewById<recyclerView >(R.id.clothList_view)
-//        for(i in 0 until clothList.list.size){
-//            val viewOfCloth=layoutInflater.inflate(R.layout.mycloset_clothes,null)
-//            val clothView=viewOfCloth.findViewById<ImageView>(R.id.cloth_image)
-////            val clothTextView=viewOfCloth.findViewById<TextView>(R.id.cloth_text)
-////            clothTextView.setText(clothList.list.get(i).name)
-//            container.addView(clothView)
-//        }
-//    }
-
 }
 
 
