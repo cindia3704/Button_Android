4a66cc7e7cfa0e4d7db7cc566637ebd184f9d920
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-09 12:50:24 +0900

    Update MainFragmentStatePagerAdapter.kt

--- app/src/main/java/com/example/button/startApp_1/adapter/MainFragmentStatePagerAdapter.kt
+++ app/src/main/java/com/example/button/startApp_1/adapter/MainFragmentStatePagerAdapter.kt
@@ -3,15 +3,15 @@ package com.example.button.startApp_1.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.button.startApp_1.fragment.CalendarFragment
-import com.example.button.startApp_1.fragment.MyclosetFragment
-import com.example.button.startApp_1.fragment.RecommendFragment
+import com.example.button.startApp_1.fragment.*
 
-class MainFragmentStatePagerAdapter(fm: FragmentManager, val fragmentCount : Int) : FragmentStatePagerAdapter(fm){
+class MainFragmentStatePagerAdapter(fm: FragmentManager, val fragmentCount : Int,val userId : Int) : FragmentStatePagerAdapter(fm){
     override fun getItem(position: Int): Fragment {
         when(position){
             0->return RecommendFragment()
-            1->return MyclosetFragment()
+            1->return MyclosetFragment.newInstance(userId)
+            2->return CoordiFragment.newInstance(userId)
+            3->return FriendFragment.newInstance(userId)
             else->return CalendarFragment()
         }
     }
