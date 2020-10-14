c15491ab4d8e6a86923002e5cd95d623f9e4535b
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-14 15:37:05 +0900

    Update ReMainActivity.kt

--- app/src/main/java/com/example/button/startApp_1/activity/ReMainActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/activity/ReMainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.button.startApp_1.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.button.R
 import com.example.button.startApp_1.adapter.MainFragmentStatePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class ReMainActivity : AppCompatActivity() {
+class  ReMainActivity : AppCompatActivity() {
 
-    private var userId: Int=1
+    var userId: Int=1
+
     private fun configureBottomNavigation(userId : Int) {
         vp_ac_main_frag_pager.adapter =
             MainFragmentStatePagerAdapter(
@@ -27,16 +28,24 @@ class ReMainActivity : AppCompatActivity() {
         tl_ac_main_bottom_menu.getTabAt(1)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_mycloset_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(2)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_selfcoordi_tab) as RelativeLayout
+        tl_ac_main_bottom_menu.getTabAt(3)!!.customView =
+            bottomNaviLayout.findViewById(R.id.btn_bottom_navi_friendlist_tab) as RelativeLayout
+        tl_ac_main_bottom_menu.getTabAt(4)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_calendar_tab) as RelativeLayout
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_re_main)
-
-
+        setContentView(R.layout.activity_main)
         userId=getIntent().getIntExtra("userId",3)
 
         configureBottomNavigation(userId)
         vp_ac_main_frag_pager.setCurrentItem(1)
     }
 }
+
+
+
+
+
