150cabc2334293eae97d5df2e3ed85702d50ce0a
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-14 15:37:55 +0900

    Update FriendAdatper.kt

--- app/src/main/java/com/example/button/startApp_1/adapter/FriendAdatper.kt
+++ app/src/main/java/com/example/button/startApp_1/adapter/FriendAdatper.kt
@@ -1,8 +1,16 @@
 package com.example.button.startApp_1.adapter
 
+import android.content.Intent
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.button.R
+import com.example.button.startApp_1.activity.AddCoordiActivity
+import com.example.button.startApp_1.activity.CoordiDetailActivity
+import com.example.button.startApp_1.data.Friend
 import com.example.button.startApp_1.data.User
 import com.example.button.startApp_1.fragment.FriendFragment
 
@@ -10,14 +18,23 @@ import com.example.button.startApp_1.fragment.FriendFragment
 class FriendAdatper(val fragment : FriendFragment) :RecyclerView.Adapter<FriendAdatper.ViewHolder>() {
 
 
-    var friendItems = mutableListOf<User>()
+    var friendItems = mutableListOf<Friend>()
 
     inner class ViewHolder(view : View)  : RecyclerView.ViewHolder(view){
 
+
+        var friendName : TextView = view.findViewById(R.id.friendName)
+        var coordi : TextView = view.findViewById(R.id.cordiFriend)
+
+
     }
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.friend_list, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -25,6 +42,16 @@ class FriendAdatper(val fragment : FriendFragment) :RecyclerView.Adapter<FriendA
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        holder.run {
+
+            friendName.setText("${friendItems[position].friendName}")
+            coordi.setOnClickListener {
+//                var intent = Intent(fragment.context,CoordiDetailActivity::class.java)
+//                intent.putExtra("KEY_FRIEND_ID",friendItems[position].frienduser)
+//                intent.putExtra("KEY_USER_ID",friendItems[position].user)
+//                fragment.startActivity(intent)
+            }
+        }
     }
 }
