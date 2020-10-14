169c302e2a94c31d485dc7c8ac85ff3336426530
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-14 15:39:59 +0900

    Create Friend.kt

---
+++ app/src/main/java/com/example/button/startApp_1/data/Friend.kt
@@ -0,0 +1,12 @@
+package com.example.button.startApp_1.data
+
+
+
+
+data class Friend(
+    var user : Int,
+    var frienduser : Int,
+    var accepted : Boolean,
+    var friendName : String?,
+    var timestamp : String
+)
