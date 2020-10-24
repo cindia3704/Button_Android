a31660f4304694802cde5aa854e502e5972e2407
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-10-24 10:58:33 +0900

    Update ReMainActivity.kt

--- app/src/main/java/com/example/button/startApp_1/activity/ReMainActivity.kt
+++ app/src/main/java/com/example/button/startApp_1/activity/ReMainActivity.kt
@@ -24,6 +24,8 @@ class  ReMainActivity : AppCompatActivity() {
 
         val bottomNaviLayout: View =
             this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
+
+
         tl_ac_main_bottom_menu.getTabAt(0)!!.customView =
             bottomNaviLayout.findViewById(R.id.btn_bottom_navi_recommend_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(1)!!.customView =
