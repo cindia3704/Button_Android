355918859b8814d092b2d91873c686d576fb1b95
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-11-14 12:00:31 +0900

    Update CalendarDayItem.kt

--- app/src/main/java/com/example/button/startApp_1/adapter/CalendarDayItem.kt
+++ app/src/main/java/com/example/button/startApp_1/adapter/CalendarDayItem.kt
@@ -5,17 +5,32 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.button.R
 import com.example.button.startApp_1.activity.CalendarCoordiRegisterActivity
+import com.example.button.startApp_1.data.CoordiList
+import com.example.button.startApp_1.data.ResInsertCoordiForCalendar
 import com.example.button.startApp_1.fragment.CalendarFragment
+import com.example.button.startApp_1.network.RetrofitClient
 
 
-class CalendarDayItem(var fragment : CalendarFragment,var year : Int,var month : Int,var userId : Int) : RecyclerView.Adapter<CalendarDayItem.ItemViewHolder>() {
+class CalendarDayItem(
+    var fragment: CalendarFragment,
+    var year: Int,
+    var month: Int,
+    var userId: Int
+) : RecyclerView.Adapter<CalendarDayItem.ItemViewHolder>() {
 
 
-
+    var outfitList = hashMapOf<String, CoordiList>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
     var items = mutableListOf<Int>()
         set(value) {
             field = value
@@ -27,15 +42,31 @@ class CalendarDayItem(var fragment : CalendarFragment,var year : Int,var month :
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         var tv_day = view.findViewById<TextView>(R.id.tv_day)
+        var ivCoordi  = view.findViewById<ImageView>(R.id.ivCoordi)
+        var cardView = view.findViewById<CardView>(R.id.cardview)
 
         fun bind(position: Int) {
-            tv_day.text = "${position + 1}"
+            var day = position+1
+            var valueDay = if(day<10) "0$day" else "$day"
+            tv_day.text = "${day}"
+
+            cardView.visibility = View.GONE
+            Log.e("test","itemviewholder year=$year-$month-$valueDay")
+            var outfitImage = outfitList["$year-$month-$valueDay"]
+            outfitImage?.let{
+                cardView.visibility = View.VISIBLE
+                Glide.with(fragment)
+                    .load(RetrofitClient.imageBaseUrl + it.clothes[0].photo)
+                    .placeholder(R.drawable.circle)
+                    .into(ivCoordi)
+            }
 
             tv_day.setOnClickListener {
-                var intent = Intent(fragment.context,CalendarCoordiRegisterActivity::class.java)
-                intent.putExtra("year",year)
-                intent.putExtra("month",month)
-                intent.putExtra("KEY_USER_ID",userId)
+                var intent = Intent(fragment.context, CalendarCoordiRegisterActivity::class.java)
+                intent.putExtra("year", year)
+                intent.putExtra("month", month)
+                intent.putExtra("day", day)
+                intent.putExtra("KEY_USER_ID", userId)
                 fragment.startActivity(intent)
             }
         }
