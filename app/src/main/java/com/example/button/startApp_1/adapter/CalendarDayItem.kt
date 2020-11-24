4fafb9ebbd4f4c252426e8e9d2fac836696c9373
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-11-24 13:44:12 +0900

    Update CalendarDayItem.kt

--- app/src/main/java/com/example/button/startApp_1/adapter/CalendarDayItem.kt
+++ app/src/main/java/com/example/button/startApp_1/adapter/CalendarDayItem.kt
@@ -1,6 +1,7 @@
 package com.example.button.startApp_1.adapter
 
 import android.content.Intent
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +17,7 @@ import com.example.button.startApp_1.data.CoordiList
 import com.example.button.startApp_1.data.ResInsertCoordiForCalendar
 import com.example.button.startApp_1.fragment.CalendarFragment
 import com.example.button.startApp_1.network.RetrofitClient
+import java.util.*
 
 
 class CalendarDayItem(
@@ -25,6 +27,19 @@ class CalendarDayItem(
     var userId: Int
 ) : RecyclerView.Adapter<CalendarDayItem.ItemViewHolder>() {
 
+
+    private var startWeek = 0
+
+    init {
+
+        var cal = Calendar.getInstance()
+        cal.set(Calendar.YEAR, year)
+        cal.set(Calendar.MONTH, month - 1)
+        cal.set(Calendar.DATE, 1)
+
+        startWeek = cal.get(Calendar.DAY_OF_WEEK) -1
+
+    }
 
     var outfitList = hashMapOf<String, CoordiList>()
         set(value) {
@@ -42,33 +57,49 @@ class CalendarDayItem(
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         var tv_day = view.findViewById<TextView>(R.id.tv_day)
-        var ivCoordi  = view.findViewById<ImageView>(R.id.ivCoordi)
+        var ivCoordi = view.findViewById<ImageView>(R.id.ivCoordi)
         var cardView = view.findViewById<CardView>(R.id.cardview)
 
         fun bind(position: Int) {
-            var day = position+1
-            var valueDay = if(day<10) "0$day" else "$day"
-            tv_day.text = "${day}"
+            if(position < startWeek){
+                tv_day.visibility = View.INVISIBLE
+                tv_day.visibility = View.INVISIBLE
+                cardView.visibility = View.INVISIBLE
+            }else{
+                var day = (position - startWeek) + 1
+                var valueDay = if (day < 10) "0$day" else "$day"
+                tv_day.text = "${day}"
 
-            cardView.visibility = View.GONE
-            Log.e("test","itemviewholder year=$year-$month-$valueDay")
-            var outfitImage = outfitList["$year-$month-$valueDay"]
-            outfitImage?.let{
-                cardView.visibility = View.VISIBLE
-                Glide.with(fragment)
-                    .load(RetrofitClient.imageBaseUrl + it.clothes[0].photo)
-                    .placeholder(R.drawable.circle)
-                    .into(ivCoordi)
+                cardView.visibility = View.GONE
+                var outfitImage = outfitList["$year-$month-$valueDay"]
+                outfitImage?.let {
+                    cardView.visibility = View.VISIBLE
+
+                    var cloth = it.clothes[0]
+                    for( item in it.clothes){
+                        if(TextUtils.equals("TOP",item.category)){
+                            cloth = item
+                            break
+                        }
+                    }
+
+                    Glide.with(fragment)
+                        .load(RetrofitClient.imageBaseUrl + cloth.photo)
+                        .placeholder(R.drawable.circle)
+                        .into(ivCoordi)
+                }
+
+                tv_day.setOnClickListener {
+                    var intent = Intent(fragment.context, CalendarCoordiRegisterActivity::class.java)
+                    intent.putExtra("year", year)
+                    intent.putExtra("month", month)
+                    intent.putExtra("day", day)
+                    intent.putExtra("KEY_USER_ID", userId)
+                    fragment.startActivity(intent)
+                }
             }
 
-            tv_day.setOnClickListener {
-                var intent = Intent(fragment.context, CalendarCoordiRegisterActivity::class.java)
-                intent.putExtra("year", year)
-                intent.putExtra("month", month)
-                intent.putExtra("day", day)
-                intent.putExtra("KEY_USER_ID", userId)
-                fragment.startActivity(intent)
-            }
+
         }
     }
 
