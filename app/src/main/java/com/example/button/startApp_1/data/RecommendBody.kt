2b2e8a36756adb8dc87f0042c5b16fcfebfacbfe
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-11-24 13:45:51 +0900

    Update RecommendBody.kt

--- app/src/main/java/com/example/button/startApp_1/data/RecommendBody.kt
+++ app/src/main/java/com/example/button/startApp_1/data/RecommendBody.kt
@@ -1,7 +1,19 @@
 package com.example.button.startApp_1.data
 
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 
-data class RecommendBody(val id: Int, val season: MutableList<String> ,val place1 : Int,val place2 : Int,val people1 : Int,val people2 : Int,val event1 : Int,val event2 : Int,val mood :Int)
+@Parcelize
+data class RecommendBody(
+    val id: Int,
+    val season: String,
+    val place1: Int,
+    val place2: Int,
+    val people1: Int,
+    val people2: Int,
+    val event1: Int,
+    val event2: Int,
+    val mood: Int
+) : Parcelable
