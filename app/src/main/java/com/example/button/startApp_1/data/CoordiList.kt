e47271fbe186766a9d77888654fbc903172024dd
sO-Omin <70512115+sO-Omin@users.noreply.github.com>
2020-11-26 10:04:45 +0900

    Update CoordiList.kt

--- app/src/main/java/com/example/button/startApp_1/data/CoordiList.kt
+++ app/src/main/java/com/example/button/startApp_1/data/CoordiList.kt
@@ -4,7 +4,7 @@ package com.example.button.startApp_1.data
 
 data class CoordiList(
     var id : Int,
-var outfitID : Int,
-var outfitName : String,
-var clothes : MutableList<Cloth>,
-var outfitBy:Int)
+    var outfitID : Int,
+    var outfitName : String,
+    var clothes : MutableList<Cloth>,
+    var outfitBy:Int)
